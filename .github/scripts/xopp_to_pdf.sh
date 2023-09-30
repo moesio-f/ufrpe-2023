b47d11c291e4198bea7fa82f21581bfc5bd899e8
Moésio Filho <moesio.f@gmail.com>
2023-09-30 18:36:11 -0300

    fix: xopp_to_pdf script
    - Fix xournal binary name;
    - Fix fname;

--- .github/scripts/xopp_to_pdf.sh
+++ .github/scripts/xopp_to_pdf.sh
@@ -4,12 +4,12 @@ set -e
 scripts_dir="$(realpath $(dirname $0))"
 github_dir="$(dirname $scripts_dir)"
 root_dir="$(dirname $github_dir)"
-xournal_dir="$root/xournal"
+xournal_dir="$root_dir/xournal"
 
 for f in $(find $xournal_dir -type f -path '**/*.xopp')
 do
-    fname="${fname%.*}.pdf"
-    xournalapp -p $fname $f
+    fname="${f%.*}.pdf"
+    xournalpp -p $fname $f
     rm $f
 done
 
